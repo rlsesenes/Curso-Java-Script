663f23b0f1620094a546ea6800e091b0ba058607
rlsesenes <112500696+rlsesenes@users.noreply.github.com>
2022-10-25 11:17:51 -0500

    listo

---
+++ datos.js
@@ -0,0 +1,20 @@
+{
+
+    "nombre": "Ramon Lozano", 
+    "Edad": 51, 
+    "nacionalidad": "mexicana"
+
+
+
+
+}
+
+var json = '{"nombre": "Ramon Lozano", "Edad": 51, "nacionalidad": "mexicana"}';
+var obj= JSON.parse(json);console.log(obj.nombre); obj.nombre="pedro";
+console.log(obj.nombre);
+var json= '{"nombre":"Manuel","Edad":43, "nacionalidad":"mexicana","hobbies":["ajedrez", "videojuegos","comics"]}';
+
+var obj = JSON.parse(json);
+console.log(obj.hobbies[0]);
+obj.hobbies[0] = "pelicula"; 
+console.log(obj.hobbies[0]);
